d06ac9eba1a922d59ac4d3816d2539d9a2926c2f
sheto <sheto@mail.top>
2020-12-10 14:23:31 +0800

    修改最开始字符匹配时属性值前面的特殊字符会超过一个的情况

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function $rebuild(_configItem){
-	let configItemName = _configItem.match(/\[(\w+)\]/)[1]
-	let configItem = _configItem.match(/(?!\s)(\w+\=.+)/g);
-	
+	console.log(arguments)
+	var configItemName = _configItem.match(/\[(\w+)\]/)[1]
+	var configItem = _configItem.match(/(?!\s)(\w+\=.+)/g);
 	return {
 		[configItemName]: configItem.map(_item=>{
 			let item = _item.split("=");
@@ -25,33 +25,37 @@ function $rebuild(_configItem){
 };
 
 function $finish(_profile){
-	let profile = {};
-	let $array2object = function(_item){
-		let name = Object.keys(_item)[0]
-		let profileDetails = {}
-		// 切除链接
-		// 模式一：递归结构
-		// profile[name] = Array.isArray(_item[name])?$finish(_item[name]):JSON.parse(JSON.stringify(_item[name]))
-		// 模式二：扁平结构
-		_item[name].forEach(function(_detail){
-			let name = Object.keys(_detail)[0]
-			profileDetails[name] = _detail[name]
-		})
-		profile[name] = profileDetails
-	};
-	_profile.forEach($array2object)
-	return profile;
+	if(_profile){
+		let profile = {};
+		let $array2object = function(_item){
+			let name = Object.keys(_item)[0]
+			let profileDetails = {}
+			// 切除链接
+			// 模式一：递归结构
+			// profile[name] = Array.isArray(_item[name])?$finish(_item[name]):JSON.parse(JSON.stringify(_item[name]))
+			// 模式二：扁平结构
+			_item[name].forEach(function(_detail){
+				let name = Object.keys(_detail)[0]
+				profileDetails[name] = _detail[name]
+			})
+			profile[name] = profileDetails
+		};
+		_profile.forEach($array2object)
+		return profile;
+	}else{
+		return {}
+	}
 }
 
 module.exports = function $read(_dir){
 	let {resolve} = require("path");
 	let {readFileSync} = require("fs");
-	let profile
+	let profile;
 	try{
-		profile = readFileSync(resolve(_dir),"utf8")
+		profile = readFileSync(resolve(process.cwd(),_dir),"utf8")
 	}catch{
 		// 如果没有配置文件
 		profile = ""
-	}
-	return $finish((profile.match(/\[\w+\](\s\w+=.+){1,}/g)||[{}]).map($rebuild))
+	};
+	return $finish((profile.match(/\[\w+\](\s+\w+=.+){1,}/g)||[]).map($rebuild))
 }
